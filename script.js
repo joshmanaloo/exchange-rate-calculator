9b38323e06c896ecf2a8a3e2d230053e65a1678d
joshmanaloo <joshuavincemanalo@gmail.com>
2020-02-15 22:02:53 -0600

    First commit and upload to GitHub

---
+++ script.js
@@ -0,0 +1,41 @@
+//create variables to grab
+const currencyEl_one = document.getElementById('currency-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_one = document.getElementById('amount-one');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// called on everything! Fetch exchange rates and update the DOM
+function calculate() {
+  const currency_one = currencyEl_one.value; //take the value available in the select tag
+  const currency_two = currencyEl_two.value;
+
+  //Use exchangeRate-API
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`) //gives promise background
+    .then(res => res.json())
+    .then(data => {
+      // console.log(data);
+      const rate = data.rates[currency_two];
+      // console.log(rate);
+
+      rateEl.innerText = `1 ${currency_one} = ${rate}`; //For displaying the current rate
+
+      amountEl_two.value = (amountEl_one.value*rate).toFixed(2); //toFixed method is for 2 decimal places
+    });
+}
+
+//Event listeners
+currencyEl_one.addEventListener('change', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+amountEl_two.addEventListener('input', calculate);
+swap.addEventListener('click', () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  calculate();
+})
+
+calculate();
